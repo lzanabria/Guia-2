044540da79bb67afd5c942983f30902e0fed3eae
lzanabria <lazanabria2@misena.edu.co>
2022-09-25 01:00:36 -0500

    Ejercicio 13

--- main.js
+++ main.js
@@ -1,14 +1,27 @@
 addEventListener("DOMContentLoaded", (e) => {
     setTimeout(() => {
-        let n1, n2, resta;
-        n1=Number(prompt("Ingrese el valor del primer número: "));
-        n2=Number(prompt("Ingrese el valor del segundo número: "));
-        if (n1>n2){
-            resta=n1-n2;
-            alert("El resultado de la resta es: "+resta);
+        let val, opc, desc, val_desc, pago;
+        val=Number(prompt("Ingrese el valor del helado: "));
+        opc=Number(prompt("Seleccione el tipo de membresía: \n▶ 1. Tipo A \n▶ 2. Tipo B \n▶ 3. Tipo C"));
+        if(opc==1){
+            desc=10;
+            val_desc=(val*desc)/100;
+            pago=val-val_desc;
+        }
+        else if(opc==2){
+            desc=15;
+            val_desc=(val*desc)/100;
+            pago=val-val_desc;
+        }
+        else if(opc==3){
+            desc=20;
+            val_desc=(val*desc)/100;
+            pago=val-val_desc;
         }
         else{
-            alert("No es posible realizar la resta, porque el primer número no es mayor que el segundo");
+            alert("¡Por favor seleccione una opción válida!");
+            return;
         }
+        alert("¡FELICIDADES HAZ RECIBIDO UN DESCUENTO DEL "+desc+"%!"+"\n▶ Valor del helado: $"+val+"\n▶ Valor del descuento: $"+val_desc+"\n▶ Valor a pagar: $"+pago);
     }, 500);
 });
