93e70f0c4d192ca576a88c634a013c18b20f1dad
lzanabria <lazanabria2@misena.edu.co>
2022-09-24 23:34:15 -0500

    Ejercicio 9

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
 addEventListener("DOMContentLoaded", (e) => {
     setTimeout(() => {
-        let opc, hrs, pago;
-        const paghr_planta=20000, paghr_admin=10000;
-        hrs=Number(prompt("Ingrese la cantidad de horas trabajadas: "));
-        opc=Number(prompt("Seleccione el tipo de empleado: \n▶ 1. Empleado de planta.\n▶ 2. Empleado administrativo."));
-        
-        if (opc==1) {
-            pago=hrs*paghr_planta;
-            alert("▶ Empleado: Planta.\n▶Pago por hora: $"+paghr_planta+"\n▶ Horas trabajadas: "+hrs+"\n▶ Pago: $"+pago);
+        let cant, sum_par=0, prod_imp=1;
+        let pares=[];
+        let impares=[];
+        cant=Number(prompt("Ingrese hasta que número desea incluir: "));
+        for(let i=1; i<=cant; i++) {
+            if(i%2==0) {
+                pares.push(i);
+                sum_par+=i;
+            } 
+            else {
+                impares.push(i);
+                prod_imp*=i;
+            }
         }
-        else if (opc==2) {
-            pago=hrs*paghr_admin;
-            alert("▶ Empleado: Administrativo.\n▶Pago por hora: $"+paghr_admin+"\n▶ Horas trabajadas: "+hrs+"\n▶ Pago: $"+pago);
-        }
-        else {
-            alert("¡Por favor seleccione una opción válida!");
-        }
+        alert("▶ Números pares: "+pares+"\n    La suma de los números pares es: "+sum_par+"\n\n▶ Numeros impares: "+impares+"\n    El producto de los números impares es: "+prod_imp);
     }, 500);
 });
