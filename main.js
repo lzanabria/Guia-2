5fd1455f37e0e3724e798fa1588052f43e4d7713
lzanabria <lazanabria2@misena.edu.co>
2022-09-24 20:20:11 -0500

    Ejercicio 8

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 addEventListener("DOMContentLoaded", (e) => {
     setTimeout(() => {
-        let coms, val_desc, pagof;
-        const desc=15;
-        coms=Number(prompt("Ingrese el valor de consumo: "));
-        val_desc=(coms*desc)/100;
-        pagof=coms-val_desc;
-        if(coms>130000) {
-            alert("¡HAZ GANADO UN DESCUENTO DEL "+desc+"%!\n▶ Su consumo es de: $"+coms+"\n▶ El valor de descuento es de: $"+val_desc+"\n▶ Su pago final es de: $"+pagof);
+        let opc, hrs, pago;
+        const paghr_planta=20000, paghr_admin=10000;
+        hrs=Number(prompt("Ingrese la cantidad de horas trabajadas: "));
+        opc=Number(prompt("Seleccione el tipo de empleado: \n▶ 1. Empleado de planta.\n▶ 2. Empleado administrativo."));
+        
+        if (opc==1) {
+            pago=hrs*paghr_planta;
+            alert("▶ Empleado: Planta.\n▶Pago por hora: $"+paghr_planta+"\n▶ Horas trabajadas: "+hrs+"\n▶ Pago: $"+pago);
+        }
+        else if (opc==2) {
+            pago=hrs*paghr_admin;
+            alert("▶ Empleado: Administrativo.\n▶Pago por hora: $"+paghr_admin+"\n▶ Horas trabajadas: "+hrs+"\n▶ Pago: $"+pago);
         }
         else {
-            alert("¡NO HAZ GANADO DESCUENTO!\n▶ El valor a pagar es de: $"+coms);
+            alert("¡Por favor seleccione una opción válida!");
         }
     }, 500);
-}); 
+});
