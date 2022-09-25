a6842a44035ff2787659cf2863382aff40aa9130
lzanabria <lazanabria2@misena.edu.co>
2022-09-24 19:32:35 -0500

    Ejercicio 5

--- main.js
+++ main.js
@@ -1,20 +1,15 @@
 addEventListener("DOMContentLoaded", (e) => {
     setTimeout(() => {
-        let opc, b_triangulo, h_triangulo, a_triangulo, r_circulo, a_circulo;
-        opc=Number(prompt("Seleccione una opción: \n▶  1. Área de un triángulo \n▶  2. Área de un círculo"));
-        if (opc==1) {
-            b_triangulo=Number(prompt("Ingrese la base del triángulo: "));
-            h_triangulo=Number(prompt("Ingrese la altura del triángulo: "));
-            a_triangulo=(b_triangulo*h_triangulo)/2;
-            alert("El área del triángulo es: "+a_triangulo);
-        }
-        else if (opc==2) {
-            r_circulo=Number(prompt("Ingrese el radio del círculo: "));
-            a_circulo=Math.PI*Math.pow(r_circulo, 2);
-            alert("El área del círculo es: "+a_circulo);
+        let b, h, a;
+        b=Number(prompt("Ingrese el valor de la base del rectángulo: "));
+        h=Number(prompt("Ingrese el valor de la altura del rectángulo: "));
+
+        if(b>0 && h>0) {
+            a=b*h;
+            alert("El área del rectángulo es: "+a);
         }
         else {
-            alert("¡Por favor seleccione una opción válida!");
+            alert("¡Haz ingresado valores negativos!\nNo se puede calcular el área del rectángulo");
         }
     }, 500);
 });
