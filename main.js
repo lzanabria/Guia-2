b431594d7327797d3d2e83ceeab769b57edd4b48
lzanabria <lazanabria2@misena.edu.co>
2022-09-24 19:16:12 -0500

    Ejercicio 2

--- main.js
+++ main.js
@@ -1,24 +1,21 @@
 addEventListener("DOMContentLoaded", (e) => {
     setTimeout(() => {
-        let num, res="";
-        num=Number(prompt("Ingrese un número: "));
-        res+="▶ Número: "+num;
-        if(num>0) {
-            res+="\n▶ Valor: Positivo";
-            if(num<100) {
-                res+="\n▶ Es menor a 100";
-            }
-            if(num==100) {
-                res+="\n▶ Es igual a 100";
-            }
-            if(num>100) {
-                res+="\n▶ Es mayor a 100";
-            }
+        let n1, n2, n3, mayor;
+        n1=Number(prompt("Ingrese el valor del primer número: "));
+        n2=Number(prompt("Ingrese el valor del segundo número: "));
+        n3=Number(prompt("Ingrese el valor del tercer número: "));
+        
+        if(n1>n2 && n1>n3){
+            mayor=n1;
         }
 
-        else {
-            res+="\n▶ Valor: Negativo\n▶ Es menor a 100";
+        if(n2>n1 && n2>n3){
+            mayor=n2;
         }
-        alert(res);
+
+        if(n3>n1 && n3>n2){
+            mayor=n3;
+        }
+        alert("▶ El numero mayor es: "+mayor);
     }, 500);
 });
