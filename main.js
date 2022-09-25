e86be087d3c5f42b1b0054a521310ffb256010a5
lzanabria <lazanabria2@misena.edu.co>
2022-09-25 00:15:47 -0500

    Ejercicio 11

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 addEventListener("DOMContentLoaded", (e) => {
     setTimeout(() => {
-        let prods, valor, subtot=0, val_iva, total;
-        const iva=19;
-        prods=Number(prompt("Ingrese la cantidad de productos: "));
-        for(let i=1; i<=prods; i++){
-            valor=Number(prompt("Ingrese el valor del producto #"+i+": "));
-            subtot+=valor;
+        let est, nom, ape, edad, nota1, nota2, nota3, prom;
+        est=Number(prompt("Ingrese el número de estudiantes: "));
+        for(let i=1; i<=est; i++){
+            nom=prompt("Ingrese el nombre del estudiante #"+i+": ");
+            ape=prompt("Ingrese el apellido del estudiante #"+i+": ");
+            edad=Number(prompt("Ingrese la edad del estudiante #"+i+": "));
+            nota1=Number(prompt("Ingrese la nota 1 del estudiante #"+i+": "));
+            nota2=Number(prompt("Ingrese la nota 2 del estudiante #"+i+": "));
+            nota3=Number(prompt("Ingrese la nota 3 del estudiante #"+i+": "));
+            prom=(nota1+nota2+nota3)/3;
+            if(prom>=7){
+                alert("El estudiante "+nom+" "+ape+" tiene "+edad+" años, su promedio es "+prom+" y está aprobado");
+            }
+            else{
+                alert("El estudiante "+nom+" "+ape+" tiene "+edad+" años, su promedio es "+prom+" y está reprobado");
+            }
         }
-        val_iva=(subtot*iva)/100;
-        total=subtot+val_iva;
-        alert("▶ El subtotal de la compra es: $"+subtot+"\n▶ El porcentaje de IVA es: "+iva+"%\n▶ El valor del IVA es: $"+val_iva+"\n▶ El total de la compra es: $"+total);
     }, 500);
 });
