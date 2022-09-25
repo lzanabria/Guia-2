7315dc3197afd8c0bb69c80a75b6ff440071b1f0
lzanabria <lazanabria2@misena.edu.co>
2022-09-24 20:01:42 -0500

    Ejercicio 7

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
 addEventListener("DOMContentLoaded", (e) => {
     setTimeout(() => {
-        let p, t, estado="";
-        p=Number(prompt("Ingrese el valor de la presión: "));
-        t=Number(prompt("Ingrese el valor de la temperatura: "));
-
-        if(p>200) {
-            estado="Alarma";
-        }
-        else if(t>100) {
-            estado="Alarma";
+        let coms, val_desc, pagof;
+        const desc=15;
+        coms=Number(prompt("Ingrese el valor de consumo: "));
+        val_desc=(coms*desc)/100;
+        pagof=coms-val_desc;
+        if(coms>130000) {
+            alert("¡HAZ GANADO UN DESCUENTO DEL "+desc+"%!\n▶ Su consumo es de: $"+coms+"\n▶ El valor de descuento es de: $"+val_desc+"\n▶ Su pago final es de: $"+pagof);
         }
         else {
-            estado="Normal";
+            alert("¡NO HAZ GANADO DESCUENTO!\n▶ El valor a pagar es de: $"+coms);
         }
-        alert("▶  El estado actual es: "+estado);
     }, 500);
-});
+}); 
